5f58954720642aa62873e418d727c282c64a723b
anup kumr <akgjune06@gmail.com>
2022-10-06 23:20:47 +0530

    sign-up-done

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,9 +7,7 @@ const Header = props =>{
         <h1 className="logoimg">
             <img src={Logo} className="log" alt='Code fraggers logo'></img>
         </h1>
-        {/* <button className="sign-Up">
-            Sign Up
-        </button> */}
+       
         <button className="signIn">
             Sign In
         </button>
