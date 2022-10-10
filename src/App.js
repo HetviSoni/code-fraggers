8bfea49d2206fedd2ba3e950d2d1f4ee799fd157
Abir Santra <santraabir8@gmail.com>
2022-10-10 21:19:07 +0530

    Commented unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/header/header";
 import HomePage from "./components/body/homepage";
-import SignupForm from "./components/signUp/SignupForm";
+// import SignupForm from "./components/signUp/SignupForm";
 import React, { Fragment } from "react";
 
 function App() {
