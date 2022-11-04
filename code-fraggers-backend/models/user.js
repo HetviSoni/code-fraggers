53049a913f51cf2e983d5daa56a93e5a33535f2e
DeVilla04 <dalalvraj21@gmail.com>
2022-11-05 00:38:14 +0530

    Contains backend for login and register

---
+++ code-fraggers-backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: { type: String, unique: true, required: true },
+    metamaskid: { type: String, unique: true, lowercase: true, required: true },
+    email: { type: String, unique: true, lowercase: true, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
